19bc5ebe2f7c3515ec44abbd298939480d95be9d
Sahil Thakur <163773985+saolz@users.noreply.github.com>
2024-12-06 18:27:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import { Analytics } from '@vercel/analytics';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World! This is my simple web app deployed on Vercel!</h1>
+      <Analytics />
+    </div>
+  );
+}
+
+export default App;
