84885a855258886fe1472c9be0d08f6933810171
Sahil Thakur <163773985+saolz@users.noreply.github.com>
2024-12-06 18:51:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-// index.js
 import express from 'express';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+  // Get the real IP from X-Forwarded-For header
   const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   res.send(`<h1>Your IP Address is: ${ip}</h1>`);
 });
